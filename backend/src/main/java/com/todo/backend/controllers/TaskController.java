f1e9da2426fbd7af48a4d0230c01295fa3aa731e
Allaf Ramon <allafr1999@gmail.com>
2025-01-28 17:37:33 -0300

    update CORS configuration in TaskController to restrict origins

--- backend/src/main/java/com/todo/backend/controllers/TaskController.java
+++ backend/src/main/java/com/todo/backend/controllers/TaskController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = {"http://localhost:3000", "http://frontend:80"})
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class TaskController {
     private final TaskService taskService;
 
